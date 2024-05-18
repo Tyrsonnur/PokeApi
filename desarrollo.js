0a0d44614478c5e644ce759eba1e3c04d72387a9
Tyrsonnur <andresf199810@hotmail.com>
2024-05-17 21:04:52 -0500

    Tercera actualizacion

--- desarrollo.js
+++ desarrollo.js
@@ -1,9 +1,12 @@
-function listaPokemon(url_pokeapi = 'https://pokeapi.co/api/v2/pokemon') {
+let boton_siguiente = document.querySelector("#btn_siguiente")
+let boton_anterior = document.querySelector("#btn_anterior")
 
+function listaPokemon(url_pokeapi = 'https://pokeapi.co/api/v2/pokemon') {
     let divPokemon = document.querySelector("#grilla-pokemon")
     let dataAPI_pokeapi = fetch(url_pokeapi)
     dataAPI_pokeapi.then(respuestaPromesa => respuestaPromesa.json())
         .then(infojson => {
+            divPokemon.innerHTML = " "
             infojson.results.forEach(pokemon => {
                 let urlPokemon = pokemon.url
                 let consumoPokemon = fetch(urlPokemon)
@@ -73,9 +76,6 @@ async function GetPkmn() {
 //cambiar_pagina_siguiente()
 
 function paginaPokemones() {
-    let boton_siguiente = document.querySelector("#btn_siguiente")
-    let boton_anterior = document.querySelector("#btn_anterior")
-
     boton_siguiente.setAttribute('data-url-pokemon', 'https://pokeapi.co/api/v2/pokemon?offset=20&limit=20')
     boton_anterior.setAttribute('data-url-pokemon', '')
 
@@ -95,3 +95,61 @@ function paginaPokemones() {
 }
 
 
+function buscar() {
+
+    document.querySelector("#formulario").addEventListener("submit", funtion(e))
+    e.preventDefault()
+    let input = e.target.buscadorTxt.value
+    let divPokemon = document.querySelector("#grilla-pokemon")
+    let urlPokemon = 'https://pokeapi.co/api/v2/pokemon/${input}'
+
+    divPokemon.innerHTML = " "
+    let consumoPokemon = fetch(urlPokemon)
+    consumoPokemon.then(respuestaPromesa => respuestaPromesa.json())
+        .then(infoPokemon => {
+            let infoTipo = ""
+            infoPokemon.types.forEach(element => {
+                infoTipo += `<li>${element.type.name}</li>`
+            });
+
+            let estadisticas = ` `
+            infoPokemon.stats.forEach(dataStats => {
+                if (dataStats.stat.name == 'hp') {
+                    estadisticas += `<div> vida: <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="${dataStats.base_stat}" aria-valuemin="0" aria-valuemax="100">
+                                <div class="progress-bar bg-success" style="width: ${dataStats.base_stat}%">${dataStats.base_stat}/100</div></div>`
+                } else if (dataStats.stat.name == 'attack') {
+                    estadisticas += `<div> ataque: <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="${dataStats.base_stat}" aria-valuemin="0" aria-valuemax="100">
+                                <div class="progress-bar bg-danger" style="width: ${dataStats.base_stat}%">${dataStats.base_stat}/100</div></div>`
+                } else if (dataStats.stat.name == 'defense') {
+                    estadisticas += `<div> defensa: <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="${dataStats.base_stat}" aria-valuemin="0" aria-valuemax="100">
+                                <div class="progress-bar bg-primary" style="width: ${dataStats.base_stat}%">${dataStats.base_stat}/100</div></div>`
+                }
+            })
+
+            divPokemon.innerHTML += `
+                        <div class="card mx-2 mt-2" style="width: 20%">
+                        <img src="${infoPokemon.sprites.front_default}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title fw-bold">${pokemon.name}</h5>
+                        <ul class="ps-2 listaStats">
+                        <li class="fw-bold"> Tipo: ${infoTipo} </li>
+                        <li class="fw-bold"> Peso: ${infoPokemon.weight} Kg. </li>
+                        <li class="fw-bold"> Altura: ${infoPokemon.height} cm.</li>
+                        <li class="fw-bold"> XP Base: ${infoPokemon.base_experience} </li>
+                        </ul>
+                                <div class=" cuadroStats"> ${estadisticas} </div>
+                                </div>
+                                </div>
+                                `
+        })
+
+}
+
+
+
+
+
+
+//buscador.addEventListener('submit', function (evento) {
+// evento.preventDefault()
+//})
